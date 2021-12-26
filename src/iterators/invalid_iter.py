1e5d11cf2c059c7978e8d3a728dbd0855772aa44
Brecht Callens <brecht.callens@outlook.com>
2021-12-26 06:46:15 +0100

    Moved help classes for use in multiple tests

---
+++ src/iterators/invalid_iter.py
@@ -0,0 +1,4 @@
+class InvalidIter:
+    """Help class that has no iterator functionality."""
+    def __init__(self, max=0):
+        pass
--- src/test_tee.py
+++ src/test_tee.py
@@ -1,29 +1,8 @@
 import itertools
 import pytest
 
-
-class InvalidIter:
-    """Help class that has no iterator functionality."""
-    def __init__(self, max=0):
-        pass
-
-
-class ValidIter:
-    """Help class that implements a valid range-like iterator."""
-    def __init__(self, max=0):
-        self.max = max
-
-    def __iter__(self):
-        self.number = 0
-        return self
-    
-    def __next__(self):
-        number = self.number
-        if number < self.max:
-            self.number += 1
-            return number
-        else:
-            raise StopIteration
+from iterators.invalid_iter import InvalidIter
+from iterators.valid_iter import ValidIter
 
 
 @pytest.mark.parametrize("input, expected", [
